485933a57a4fafb5f62ad304710398d238afde3c
GitHub-04-09-21 <GitHub-2021-07-12@yandex.ru>
2021-11-16 15:58:09 +0300

    2.2.3

--- SkysmartSolver.user.js
+++ SkysmartSolver.user.js
@@ -14,7 +14,7 @@
 (async () => {
   // let Main = await import('http://localhost/Tests/SkysmartSolver/Main/Main.js');
   let Main = await import('https://nerkchere5.github.io/edu/Main/Main.js');
-  Main.Bot.task_delays = [1000, 2000];
+  Main.Bot.task_delays = [5000, 10000];
   Main.Bot.urls.referalLink = 'https://edu.skysmart.ru/contest/english_high_2021/fitulonedepazaro';
   Main.Bot.user_class_nums = [9, 10, 11];
   Main.main();

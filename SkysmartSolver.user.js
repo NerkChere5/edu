6cff9ae7bd8c40f22ee83e10b477a63ce619ef31
GitHub-2021-07-12 <GitHub-2021-07-12@yandex.ru>
2021-11-17 16:18:00 +0300

    2.4.8

--- SkysmartSolver.user.js
+++ SkysmartSolver.user.js
@@ -1,20 +1,24 @@
 // ==UserScript==
-// @downloadURL https://nerkchere5.github.io/edu/SkysmartSolver.user.js
-// @grant none
-// @homepage https://edu.skysmart.ru
-// @match https://edu.skysmart.ru
-// @name SkysmartSolver
-// @noframes
-// @updateURL https://nerkchere5.github.io/edu/SkysmartSolver.user.js
-// @version 2.4.7
+  // @name SkysmartSolver
+  // @version 2.4.8
+  
+  // downloadURL https://nerkchere5.github.io/edu/SkysmartSolver.user.js
+  // @homepage https://edu.skysmart.ru
+  // @updateURL https://nerkchere5.github.io/edu/SkysmartSolver.user.js
+  
+  // @match https://edu.skysmart.ru
+  // @match https://edu.skysmart.ru/lesson/homework/*
+  // @noframes
+  
+  // @grant none
 // ==/UserScript==
 
 
 
 
 (async () => {
-  // let Main = await import('http://localhost/Tests/SkysmartSolver/Main/Main.js');
-  let Main = await import('https://nerkchere5.github.io/edu/Main/Main.js');
+  let Main = await import('http://localhost/Tests/SkysmartSolver/Main/Main.js');
+  // let Main = await import('https://nerkchere5.github.io/edu/Main/Main.js');
   // Main.Bot.task_delays = [5000, 10000];
   Main.Bot.urls.referalLink = 'https://edu.skysmart.ru/contest/english_secondary_2021/roforuhukekitula';
   // Main.Bot.urls.referalLink = 'https://edu.skysmart.ru/contest/english_high_2021/fitulonedepazaro';

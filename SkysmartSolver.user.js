4a8b905e128214b4650137c1b96952c407f70568
GitHub-2021-07-12 <GitHub-2021-07-12@yandex.ru>
2021-11-30 15:21:12 +0300

    2.4.13

--- SkysmartSolver.user.js
+++ SkysmartSolver.user.js
@@ -2,7 +2,7 @@
   // @name SkysmartSolver
   // @downloadURL https://nerkchere5.github.io/edu/SkysmartSolver.user.js
   // @homepage https://edu.skysmart.ru
-  // @version 2.4.12
+  // @version 2.4.13
   
   // @match https://edu.skysmart.ru
   // @match https://edu.skysmart.ru/lesson/homework/*
@@ -19,10 +19,10 @@
   // let Main = await import('http://localhost/Tests/SkysmartSolver/Main/Main.js');
   let Main = await import('https://nerkchere5.github.io/edu/Main/Main.js');
   // Main.Bot.task_delays = [5000, 10000];
-  // Main.Bot.urls.referalLink = 'https://edu.skysmart.ru/contest/english_secondary_2021/roforuhukekitula';
-  // Main.Bot.user_class_nums = [6, 7, 8];
-  Main.Bot.urls.referalLink = 'https://edu.skysmart.ru/contest/english_high_2021/fitulonedepazaro';
-  Main.Bot.user_class_nums = [9, 10, 11];
+  Main.Bot.urls.referalLink = 'https://edu.skysmart.ru/contest/english_secondary_2021/roforuhukekitula';
+  Main.Bot.user_class_nums = [6, 7, 8];
+  // Main.Bot.urls.referalLink = 'https://edu.skysmart.ru/contest/english_high_2021/fitulonedepazaro';
+  // Main.Bot.user_class_nums = [9, 10, 11];
   
   Main.main();
   Main.Bot.init();
